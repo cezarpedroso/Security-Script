3dd06eb4bf1560fbca53615f1b672dae12bd8e12
Cezar Pedroso <81267812+cezarpedroso@users.noreply.github.com>
2025-06-18 22:47:25 -0500

    Update WindowsScript.bat

--- WindowsScript.bat
+++ WindowsScript.bat
@@ -7,7 +7,7 @@ function Check-OpenPorts {
     netstat -ano | Select-String "LISTEN"
 }
 
-# Function to check for users with Administrator privileges
+:: Function to check for users with Administrator privileges
 function Check-AdminUsers {
     Write-Host "Checking users with Administrator privileges..."
     $admins = Get-LocalGroupMember -Group "Administrators"
@@ -16,18 +16,18 @@ function Check-AdminUsers {
     }
 }
 
-# Function to check for sensitive files
+:: Function to check for sensitive files
 function Check-SensitiveFiles {
     Write-Host "Checking for sensitive files..."
 
-    # Check if System32 folder exists
+    :: Check if System32 folder exists
     if (Test-Path "C:\Windows\System32") {
         Write-Host "System32 folder exists"
     } else {
         Write-Host "System32 folder does not exist"
     }
 
-    # Check if any other sensitive files exist
+    :: Check if any other sensitive files exist
     $sensitiveFiles = @("C:\Windows\System32\config", "C:\Windows\System32\drivers")
     foreach ($file in $sensitiveFiles) {
         if (Test-Path $file) {
@@ -38,7 +38,7 @@ function Check-SensitiveFiles {
     }
 }
 
-# Function to check the firewall status
+:: Function to check the firewall status
 function Check-FirewallStatus {
     Write-Host "Checking firewall status..."
     $firewall = Get-NetFirewallProfile | Select-Object Name, Enabled
@@ -47,7 +47,7 @@ function Check-FirewallStatus {
     }
 }
 
-# Function to check for outdated Windows updates
+:: Function to check for outdated Windows updates
 function Check-OutdatedUpdates {
     Write-Host "Checking for outdated Windows updates..."
     $updates = Get-WmiObject -Class "Win32_QuickFixEngineering" | Sort-Object -Property InstalledOn
@@ -59,7 +59,7 @@ function Check-OutdatedUpdates {
     }
 }
 
-# Run the functions
+:: Run the functions
 Check-OpenPorts
 Check-AdminUsers
 Check-SensitiveFiles
